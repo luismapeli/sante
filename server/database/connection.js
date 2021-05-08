132ef3e61dd6bab75448ebc0ef2f0728d363e18d
Murilo Barreto <barreto_11almeida@hotmail.com>
2021-05-08 01:22:10 -0300

    String de conexao alterado

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () =>{
     try{
         //String de Conexao com mongoDB
-        const con = await mongoose.connect("mongodb+srv://user_mtest:268450@cluster0.k8lca.mongodb.net/teste?retryWrites=true&w=majority", {
+        const con = await mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser:true,
             useUnifiedTopology:true,
             useFindAndModify: false,
